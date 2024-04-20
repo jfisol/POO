c0e6de64f88ca3147be227b1efd6ad4869694972
Jose Figueroa <taodejoju@gmail.com>
2024-04-20 16:16:13 -0600

    Ejempli n1 como crear clases

---
+++ 1-Clases/Ccalculadora.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_Clases
+{
+    internal class Ccalculadora
+    {
+        //Declarar variables
+        private int numero1;
+        private int numero2;
+        private int resultado;
+
+        //Constructor
+
+        public Ccalculadora(int numero1, int numero2)
+        {
+            this.numero1 = numero1;
+            this.numero2 = numero2;
+        }
+
+        public int suma()
+        {
+            resultado = this.numero1 + this.numero2;
+            return resultado;
+        }
+
+    }
+}
---
+++ 1-Clases/Program.cs
@@ -0,0 +1,12 @@
+﻿
+using _1_Clases;
+
+Console.WriteLine("Calculadora : EJEMPLO USO DE CLASES.");
+
+
+Console.WriteLine("Suma:\n");
+
+Ccalculadora sumar = new Ccalculadora(10,20);
+
+int suma = sumar.suma();
+Console.WriteLine($"La suma total es: " + suma);
