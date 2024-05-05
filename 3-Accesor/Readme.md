f5420e9be123ee194285bc861a6b7dde83855105
Jose Figueroa <taodejoju@gmail.com>
2024-05-05 10:19:45 -0600

    Ejemplo de accesors get y set

---
+++ 3-Accesor/Readme.md
@@ -0,0 +1,5 @@
+# Accesors
+Son metodos para tener acceso para enviar o requerir informacion, esto para mantener de forma correcta el data hiding
+
+Muteitor o mutador hace una accion que cambia el estado interno del objeto.
+
---
+++ 3-Accesor/cEmpleado.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_Accesor
+{
+    internal class cEmpleado
+    {
+        private double sueldo;
+        private double impuesto;
+        
+
+
+        public void Muestra()
+        {
+            Console.WriteLine($"Sueldo = {sueldo} impuesto = {impuesto} ");
+        }
+
+        //Accesor get y set
+
+        public void set_sueldo( double sueldo )
+        {
+            this.sueldo = sueldo;
+        }
+        public double get_sueldo()
+        {
+            return this.sueldo;
+        }
+
+        public void set_impuesto( double impuesto ) 
+        {
+           this.impuesto = impuesto;
+        }
+
+        public double get_impuesto()
+        {
+            return this.impuesto;
+        }
+
+        public void calcularImpuesto()
+        {
+            impuesto = this.sueldo * 0.16;
+        }
+
+    }
+}
